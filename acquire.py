69db2108b13b0a94fb2a619c10caa065df7692d8
Aswathy Radha <aswathy.radha@gmail.com>
2023-08-23 19:44:07 -0500

    adding acquire.py

--- acquire.py
+++ acquire.py
@@ -13,19 +13,22 @@
 # ---
 
 # +
-
-import os
+#------------------- Tabular data imports------------------- 
 import pandas as pd
 import numpy as np
+#-------------------Import custom library ------------------- 
 import env
+#-------------------Import operating system interactions------
+import os
+#-------------------Import SQLAlchemy--------------------------
 from sqlalchemy import create_engine, text
 
 
 
 def get_telco_data(database='telco_churn',user=env.user, password=env.password, host=env.host):
     '''
-    Grabs telco data from codeup mySql database.
-    Returns as dataframe.
+    Grabs telco data from codeup mySQL database and returns a dataframe
+    
     '''
     query ='SELECT * FROM customers AS cc LEFT OUTER JOIN customer_subscriptions AS cs ON cc.customer_id = cs.customer_id\
         LEFT OUTER JOIN internet_service_types AS ist ON cc.internet_service_type_id = ist.internet_service_type_id\
